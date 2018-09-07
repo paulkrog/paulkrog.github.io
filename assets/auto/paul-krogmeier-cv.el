f826824ec2a9ab1a90150abed42c6329d80f3069
Paul Krogmeier <pkrog27@gmail.com>
2018-09-06 22:12:56 -0500

    updated CV

--- assets/auto/paul-krogmeier-cv.el
+++ assets/auto/paul-krogmeier-cv.el
@@ -2,13 +2,13 @@
  "paul-krogmeier-cv"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("moderncv" "10pt" "a4paper" "sans")))
+                     '(("moderncv" "12pt" "a4paper" "sans")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("geometry" "scale=0.75")))
+                     '(("inputenc" "utf8") ("geometry" "scale=0.80")))
    (TeX-run-style-hooks
     "latex2e"
     "moderncv"
-    "moderncv10"
+    "moderncv12"
     "inputenc"
     "geometry"
     "import")
