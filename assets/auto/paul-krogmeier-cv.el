7812a36d71b93e4b31b8650ae06f479ad33daec2
Paul Krogmeier <paulmk2@illinois.edu>
2022-10-27 10:42:58 -0500

    cv

--- assets/auto/paul-krogmeier-cv.el
+++ assets/auto/paul-krogmeier-cv.el
@@ -17,6 +17,9 @@
     '("MYhref" ["argument"] 2))
    (LaTeX-add-labels
     "sec:teaching")
+   (LaTeX-add-environments
+    '("slogan" LaTeX-env-args ["argument"] 0)
+    '("theme" LaTeX-env-args ["argument"] 0))
    (LaTeX-add-xcolor-definecolors
     "assassinblue"
     "assassinred"))
